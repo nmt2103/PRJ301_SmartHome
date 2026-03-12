7c53ec0330c1a3cd685ce0d36f60a3c3b2abed44
nmt <nmthuan0321@gmail.com>
2026-03-12 22:59:22 +0700

    Change DAO

--- src/java/dao/RoomDAO.java
+++ src/java/dao/RoomDAO.java
@@ -10,63 +10,65 @@ import java.util.List;
 import util.DBUtils;
 
 public class RoomDAO {
-    
-    public boolean insertRoom(RoomDTO room) {
-        String sql = "INSERT INTO ROOM (HOME_ID, NAME, FLOOR, TYPE, STATUS) VALUES (?, ?, ?, ?, ?)";
-        
-        try ( Connection conn = DBUtils.getConnection();  PreparedStatement ptm = conn.prepareStatement(sql)) {
-            
-            ptm.setInt(1, room.getHomeId().getId());
-            ptm.setString(2, room.getName());
-            ptm.setInt(3, room.getFloor());
-            ptm.setString(4, room.getType());
-            ptm.setString(5, room.getStatus());
-            
-            return ptm.executeUpdate() > 0;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return false;
+
+  public boolean insertRoom(RoomDTO room) {
+    String sql = "INSERT INTO ROOM (HOME_ID, NAME, FLOOR, TYPE, STATUS) VALUES (?, ?, ?, ?, ?)";
+
+    try ( Connection conn = DBUtils.getConnection();
+             PreparedStatement ptm = conn.prepareStatement(sql)) {
+
+      ptm.setInt(1, room.getHomeId());
+      ptm.setString(2, room.getName());
+      ptm.setInt(3, room.getFloor());
+      ptm.setString(4, room.getType());
+      ptm.setString(5, room.getStatus());
+
+      return ptm.executeUpdate() > 0;
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    
-    public List<RoomDTO> getRooms(String name, String status) {
-        List<RoomDTO> searchedList = new ArrayList<>();
-        StringBuilder sql = new StringBuilder("SELECT * FROM ROOM WHERE 1=1");
-        List<Object> parameters = new ArrayList<>();
-        
-        if (name != null && !name.trim().isEmpty()) {
-            sql.append(" AND NAME LIKE ?");
-            parameters.add("%" + name + "%");
-        }
-        
-        if (status != null & !status.trim().isEmpty()) {
-            sql.append(" AND STATUS = ?");
-            parameters.add(status);
-        }
-        
-        try ( Connection conn = DBUtils.getConnection();  PreparedStatement ptm = conn.prepareStatement(sql.toString())) {
-            
-            for (int i = 0; i < parameters.size(); i++) {
-                ptm.setObject(i + 1, parameters.get(i));
-            }
-            
-            ResultSet rs = ptm.executeQuery();
-            while (rs.next()) {
-                HomeDTO home = new HomeDTO();
-                home.setId(rs.getInt("HOME_ID"));
-                home.setName(name);
-                searchedList.add(new RoomDTO(
-                        rs.getInt("ID"), home,
-                        rs.getString("NAME"), rs.getInt("FLOOR"),
-                        rs.getString("TYPE"), rs.getString("STATUS")));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        return searchedList;
+
+    return false;
+  }
+
+  public List<RoomDTO> getRooms(String name, String status) {
+    List<RoomDTO> searchedList = new ArrayList<>();
+    StringBuilder sql = new StringBuilder("SELECT * FROM ROOM WHERE 1=1");
+    List<Object> parameters = new ArrayList<>();
+
+    if (name != null && !name.trim().isEmpty()) {
+      sql.append(" AND NAME LIKE ?");
+      parameters.add("%" + name + "%");
     }
+
+    if (status != null & !status.trim().isEmpty()) {
+      sql.append(" AND STATUS = ?");
+      parameters.add(status);
+    }
+
+    try ( Connection conn = DBUtils.getConnection();
+             PreparedStatement ptm = conn.prepareStatement(sql.toString())) {
+
+      for (int i = 0; i < parameters.size(); i++) {
+        ptm.setObject(i + 1, parameters.get(i));
+      }
+
+      ResultSet rs = ptm.executeQuery();
+      while (rs.next()) {
+        HomeDTO home = new HomeDTO();
+        home.setId(rs.getInt("HOME_ID"));
+        home.setName(name);
+        searchedList.add(new RoomDTO(
+                rs.getInt("ID"), rs.getInt("HOME_ID"),
+                rs.getString("NAME"), rs.getInt("FLOOR"),
+                rs.getString("TYPE"), rs.getString("STATUS")));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return searchedList;
+  }
 
   public RoomDTO getRoomById(String id) {
     RoomDTO room = null;
